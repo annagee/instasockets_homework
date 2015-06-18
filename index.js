5a6db16c274ae4e7593e45bd9b2d8706c9564519
Anna Gaughan <agaughan@gmail.com>
2015-06-18 09:32:32 +0100

    final commit

--- index.js
+++ index.js
@@ -2,20 +2,31 @@ var express = require('express');
 var app = express();
 var Instagram = require('instagram-node-lib');
 var http = require('http');
+var morgan = require('morgan');
 var bodyParser = require("body-parser");
 server = http.createServer(app); // Create an HTTP server.
 port = process.env.PORT || 3000; // Listen on the default port, or on 4000 if there's not one.
+var io = require('socket.io').listen(server);
 
+
+Instagram.set('client_id', process.env.INSTAGRAM_CLIENT_ID);
+Instagram.set('client_secret', process.env.INTAGRAM_CLIENT_SECRET);
+Instagram.set('callback_url', 'http://b671d092.ngrok.io/callback');
+Instagram.set('redirect_uri', 'http://b671d092.ngrok.io/');
+Instagram.set('maxSockets', 10);
+
+app.use(morgan('dev'));
 app.set('views', './views');
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(req,res){
   res.render('index')
-})
+});
 app.get('/callback', function(req, res){
-  Instagram.subscriptions.handshake(req, res); 
-})
+   Instagram.subscriptions.handshake(req, res); 
+});
 
 // app.get('/callback', function(request, response){
 //   console.log(data)
@@ -31,11 +42,6 @@ app.post('/callback', function(req, res){
   // res.end();
 });
 
-Instagram.set('client_id', process.env.INSTAGRAM_CLIENT_ID);
-Instagram.set('client_secret', process.env.INTAGRAM_CLIENT_SECRET);
-Instagram.set('callback_url', 'http://b671d092.ngrok.io');
-// Instagram.set('redirect_uri', 'YOUR-REDIRECT-URI');
-Instagram.set('maxSockets', 10);
 
 
 
@@ -57,11 +63,28 @@ Instagram.subscriptions.subscribe({
 
 server.listen(port, function() {
   console.log('Server started on http://localhost:' + port);
+  console.log('working');
   });
 
-var io = require('socket.io').listen(server);
+// var io = require('socket.io').listen(server);
 
 
-io.sockets.on('instagram', function(socket) {
-    socket.emit('instagram');
+io.sockets.on('connetion', function(socket) {
+   Instagram.tags.recent({
+    name:'blue',
+    complete: function(data){
+      socket.emit('firstShow', {firstShow: data});
+    }
+   })
+  
 });
+
+
+// io.sockets.on('connection', function (socket) {
+//   Instagram.tags.recent({ 
+//       name: 'lollapalooza',
+//       complete: function(data) {
+//         socket.emit('firstShow', { firstShow: data });
+//       }
+//   });
+// });
--- public/app.js
+++ public/app.js
@@ -1,10 +1,45 @@
 var socket = io.connect('http://b671d092.ngrok.io');
-socket.on('instagram', function() {
+socket.on('connected', function() {
    alert("Connected!");
 });
 
-$.ajax({
-      url: 'https://api.instagram.com/v1/tags/' + obj[0].object_id +'/media/recent?client_id=' + '4723704c5af04fcbbe835ae0a27a29bb',
-      crossdomain: true,
-      dataType: 'jsonp'
-    }).done 
+
+
+mostRecent: function() {
+    socket.on('firstShow', function (data) {
+        var
+            query = data,
+            source = $('#firstShow-tpl').html(),
+            compiledTemplate = Handlebars.compile(source),
+            result = compiledTemplate(query),
+            imgWrap = $('#imgContent');
+
+        imgWrap.html(result);
+    });
+},
+
+getData: function() {
+    var self = this;
+    socket.on('show', function(data) {
+        var url = data.show;
+        $.ajax({
+            url: url,
+            type: 'POST',
+            crossDomain: true,
+            dataType: 'jsonp'
+        }).done(function (data) {
+            self.renderTemplate(data);
+        }); 
+    });
+}
+// $(document).ready(function() {
+// $.ajax({
+//       url: 'https://api.instagram.com/v1/tags/' + obj[0].object_id +'/media/recent?client_id=' + '4723704c5af04fcbbe835ae0a27a29bb',
+//       crossdomain: true,
+//       dataType: 'jsonp'
+//     }).done   
+
+// });
+
+
+
